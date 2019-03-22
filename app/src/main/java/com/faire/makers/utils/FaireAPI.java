40f6cba5b50c2e23054d9a5728080c79c105912f
Ygor Pereira <ygorp@motorola.com>
2019-03-22 09:55:46 -0300

    Fix ProtoBuff definitions

--- app/src/main/java/com/faire/makers/utils/FaireAPI.java
+++ app/src/main/java/com/faire/makers/utils/FaireAPI.java
@@ -1,9 +1,17 @@
 package com.faire.makers.utils;
 
+import com.faire.makers.model.Messages.*;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Class responsable for connecting with Faire API,
@@ -17,7 +25,35 @@ public class FaireAPI {
     private static final String FILTER_RESPONSE = "SearchMakersWithFiltersResponse";
     private static final MediaType JSON_TYPE = MediaType.get("application/json; charset=utf-8");
 
-    private OkHttpClient http = new OkHttpClient();
+    private OkHttpClient okHttpClient = new OkHttpClient();
     private Gson gson = new Gson();
 
+    public FaireAPI(){
+
+    }
+
+    public List<Category> getAllCategories(){
+        List<Category> result = null;
+        try {
+            Request request = new Request.Builder().url(CATGORORIES_URL).build();
+            Call call = okHttpClient.newCall(request);
+            Response response = call.execute();
+
+            if (response.isSuccessful()) {
+                ResponseBody responseBody = response.body();
+                if (responseBody != null) {
+                    Categories categories = Categories.parseFrom(responseBody.byteStream());
+                    result = categories.getCategoriesList();
+                }
+            }
+
+            if(result == null) {
+                result = new ArrayList<>();
+            }
+        } catch (Exception e) {
+            result = new ArrayList<>();
+        }
+
+        return result;
+    }
 }
